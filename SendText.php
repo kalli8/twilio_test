1a29b9152fae7cdd79fd69f8db8233fd6d05a62b
Kalli Martin <kalli8@vt.edu>
2016-02-20 05:44:49 -0500

    first files

---
+++ SendText.php
@@ -0,0 +1,54 @@
+<?php
+    /* Send an SMS using Twilio. You can run this file 3 different ways:
+     *
+     * - Save it as sendnotifications.php and at the command line, run 
+     *        php sendnotifications.php
+     *
+     * - Upload it to a web host and load mywebhost.com/sendnotifications.php 
+     *   in a web browser.
+     * - Download a local server like WAMP, MAMP or XAMPP. Point the web root 
+     *   directory to the folder containing this file, and load 
+     *   localhost:8888/sendnotifications.php in a web browser.
+     */
+ 
+    // Step 1: Download the Twilio-PHP library from twilio.com/docs/libraries, 
+    // and move it into the folder containing this file.
+
+    require "twilio-php-master/Services/Twilio.php";
+ 
+    // Step 2: set our AccountSid and AuthToken from www.twilio.com/user/account
+    $AccountSid = "ACb4601ebfba5a649c4aae1ccbf6b1ee8f";
+    $AuthToken = "a4d6a5026b373ccd8cd7405d8026adfa";
+ 
+    // Step 3: instantiate a new Twilio Rest Client
+    $client = new Services_Twilio($AccountSid, $AuthToken);
+ 
+    // Step 4: make an array of people we know, to send them a message. 
+    // Feel free to change/add your own phone number and name here.
+    $people = array(
+        "+17033444899" => "Kalli",
+        "+16164321090" => "Michael",
+        "+12154311593" => "Miguel",
+    );
+ 
+    // Step 5: Loop over all our friends. $number is a phone number above, and 
+    // $name is the name next to it
+    foreach ($people as $number => $name) {
+ 
+        $sms = $client->account->messages->sendMessage(
+ 
+        // Step 6: Change the 'From' number below to be a valid Twilio number 
+        // that you've purchased, or the (deprecated) Sandbox number
+            "703-828-0845", 
+ 
+            // the number we are sending to - Any phone number
+            $number,
+ 
+            // the sms body
+            "Hey $name, Monkey Party at 6PM. Bring Bananas!"
+        );
+ 
+        // Display a confirmation message on the screen
+        echo "Sent message to $name";
+    }
+?>
